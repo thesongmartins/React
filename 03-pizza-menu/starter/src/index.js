66e46792690f73adc143a1f4026aa37e608256a2
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-22 22:11:10 +0100

    LockedIn 2025: Javascript logic in components

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -78,7 +78,17 @@ const Menu = () => {
 
 const Footer = () => {
   const hour = new Date().getHours();
-  console.log(hour);
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   console.log(`We're currently Open `);
+  // } else {
+  //   console.log(`Sorry, we're closed`);
+  // }
+
   return (
     <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
   );
