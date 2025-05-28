423c51a8eefc13adeee75037b7a7f70f49a7a1b7
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-27 17:57:22 -0700

    LockedIn 2025: Building steps component

---
+++ 04-steps/starter/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ background: "#7950f2", color: "fff" }}>
+          Previous
+        </button>
+        <button style={{ background: "#7950f2", color: "fff" }}>Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
