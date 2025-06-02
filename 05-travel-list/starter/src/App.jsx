9ce5c5add3b44e02cd2c68d7a7c836e4a1f4da44
thesongmartins <martinsjehoshaphat@gmail.com>
2025-06-02 20:03:11 +0100

    LockedIn 2025: Controlled Elements

--- 05-travel-list/starter/src/App.jsx
+++ 05-travel-list/starter/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -18,27 +20,47 @@ export default function App() {
 function Logo() {
   return <h1>🌴Far Away🧳</h1>;
 }
+
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(e);
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   };
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
+
 function ParkingList() {
   return (
     <div className="list">
@@ -50,6 +72,7 @@ function ParkingList() {
     </div>
   );
 }
+
 function Item({ item }) {
   return (
     <li>
@@ -60,6 +83,7 @@ function Item({ item }) {
     </li>
   );
 }
+
 function Stats() {
   return (
     <footer className="stats">
