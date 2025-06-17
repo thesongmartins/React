9e1c913b144335de4f1864ef0d319abbdc06eb20
thesongmartins <martinsjehoshaphat@gmail.com>
2025-06-17 16:57:20 +0100

    LockedIn 2025: Calculating statistics as derived states

--- 05-travel-list/starter/src/App.jsx
+++ 05-travel-list/starter/src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -117,10 +117,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p>
+        <em>Start adding items to your packing list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      You have X items on your list, and you already packed X (x%)
+      <em>
+        {percentage === 100
+          ? `You got everything! Ready to go'}`
+          : `You have ${numItems} items on your list, and you already packed ${numPacked}
+      (${percentage}%)`}
+      </em>
     </footer>
   );
 }
