631669fcdc28c0c0d4583055e5591949c8a7f014
thesongmartins <martinsjehoshaphat@gmail.com>
2026-02-21 10:46:21 +0100

    feat: adding states

--- 10-react-quiz/starter/src/App.jsx
+++ 10-react-quiz/starter/src/App.jsx
@@ -1,10 +1,56 @@
-import DateCounter from "../src/DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import Maincom from "./maincom";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+  secondsRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    async function fetchQuestions() {
+      const res = await fetch("http://localhost:3000/questions");
+      const data = await res.json();
+
+      if (!data.ok) throw new Error("Failed to fetch questions");
+      console.log(data);
+    }
+    fetchQuestions();
+  }, []);
   return (
     <>
-      <div>
-        <DateCounter />
+      <div className="app">
+        <Header />
+
+        <Maincom>
+          <p>1/15</p>
+          <p>Question?</p>
+        </Maincom>
       </div>
     </>
   );
---
+++ 10-react-quiz/starter/src/maincom.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function maincom({ children }) {
+  return <main className="main">{children}</main>;
+}
