e8edcfe8b11b4867d74f5d3ed89da2276381083e
thesongmartins <martinsjehoshaphat@gmail.com>
2026-04-18 11:27:28 +0100

    pages & Navs created

---
+++ 11-worldwise/starter/components/AppNav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppNav() {
+  return <nav>AppNav</nav>;
+}
+
+export default AppNav;
--- 11-worldwise/starter/src/App.jsx
+++ 11-worldwise/starter/src/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
