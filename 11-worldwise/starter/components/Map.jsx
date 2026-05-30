e7411c2f754b8aeb38045e115a168b6982763126
thesongmartins <martinsjehoshaphat@gmail.com>
2026-05-30 16:43:35 +0100

    Implemented County list, stored state in url, dynamic routes with url parameters & setting and reading query string

--- 11-worldwise/starter/components/Map.jsx
+++ 11-worldwise/starter/components/Map.jsx
@@ -1,8 +1,15 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   return (
     <div className={styles.mapContainer}>
       <h1>Map</h1>
+      <h1>
+        position: {lat} {lng}
+      </h1>
     </div>
   );
 }
--- 11-worldwise/starter/src/App.jsx
+++ 11-worldwise/starter/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import Product from "../pages/Product";
 import Pricing from "../pages/Pricing";
 import Homepage from "../pages/Homepage";
@@ -6,7 +8,9 @@ import PageNotFound from "../pages/PageNotFound";
 import AppLayout from "../pages/AppLayout";
 import Login from "../pages/Login";
 import CityList from "../components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "../components/CountryList";
+import City from "../components/City";
+import Form from "../components/Form";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -46,9 +50,13 @@ export default function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
 
-          <Route path="countries" element={<p>Countries</p>} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
